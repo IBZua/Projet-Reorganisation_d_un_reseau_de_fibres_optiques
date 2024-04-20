26c92c5041d778e39ec60f55cfbaeecef2352ff8
IBZua <120250986+IBZua@users.noreply.github.com>
2024-04-20 22:11:40 +0200

    Update reseau.c
    Maj des fonctions rechercheCreeNoeudListe et reconstitueReseauListe

--- reseau.c
+++ reseau.c
@@ -1,5 +1,7 @@
 #include "Reseau.h"
+#include "Chaine.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 Noeud* rechercheCreeNoeudListe(Reseau *R, double x, double y){
 	while(R->noeuds->nd && R->noeuds->nd->x != x && R->noeuds->nd->y != y){
@@ -31,20 +33,15 @@ Reseau* reconstitueReseauListe(Chaines *C){
 		prec = C->chaines->points;
 		C->chaines->points = C->chaines->points->suiv;
 		
-		while(C->chaines->points){
-			if(C->chaines->points->suiv != NULL){
+		while(C->chaines->points->suiv != NULL){
 				R->noeuds->nd = rechercheCreeNoeudListe(R, C->chaines->points->x, C->chaines->points->y);
 				R->noeuds->nd->voisins->nd = rechercheCreeNoeudListe(R, prec->x, prec->y);
 				R->noeuds->nd->voisins->suiv->nd = rechercheCreeNoeudListe(R, C->chaines->points->suiv->x, C->chaines->points->suiv->y);
 				prec = C->chaines->points;
 				C->chaines->points = C->chaines->points->suiv;
-			}
-			else{
-				break;
-			}
+		
 		}
 		
-		C->chaines->points = C->chaines->points->suiv;
 		R->commodites->extrB = rechercheCreeNoeudListe(R, C->chaines->points->x, C->chaines->points->y);
 		R->commodites = R->commodites->suiv;
 		C->chaines = C->chaines->suiv;
