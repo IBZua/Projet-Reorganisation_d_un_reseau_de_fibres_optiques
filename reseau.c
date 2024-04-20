bdd91cbc205966a8fdd3d171e78357de50dd7a03
IBZua <120250986+IBZua@users.noreply.github.com>
2024-04-20 22:02:47 +0200

    Update reseau.c
    ajout des fonction nbCommodites, nbLiaisons, ecrire, affiche

--- reseau.c
+++ reseau.c
@@ -51,3 +51,74 @@ Reseau* reconstitueReseauListe(Chaines *C){
 	}
 	return R;
 }
+
+int nbCommodites(Reseau *R){
+	int cpt = 0;
+	while(R->commodites){
+		cpt++;
+		R->commodites = R->commodites->suiv;
+	}
+	return cpt;
+}
+
+int nbLiaisons(Reseau*R){
+	int cpt = 0;
+	while(R->noeuds){
+		cpt++;
+		R->noeuds = R->noeuds->suiv;
+	}
+	return cpt;
+}
+
+void ecrireReseau(Reseau*R,FILE*f){
+	fprintf(f,"NbNoeuds: %d\n",R->nbNoeuds);
+	int nbLiaison = nbLiaisons(R);
+	int nbCommodite = nbCommodites(R);
+	fprintf(f,"NbLiaisons: %d\n",nbLiaison);
+	fprintf(f,"NbCommodites: %d\n",nbCommodite);
+	fprintf(f,"Gamma: %d\n",R->gamma);
+	fprintf(f,"\n");
+	//la liste des noeuds
+	for(int i = 0; i < R->nbNoeuds; i++){
+		fprintf(f,"v %d %f %f\n", R->noeuds->nd->num, R->noeuds->nd->x, R->noeuds->nd->y);
+	}
+	fprintf(f,"\n");
+	//la liste des voisins de chaque noeud
+	for(int i = 0; i < R->nbNoeuds; i++){
+		while(R->noeuds->nd->voisins){
+			fprintf(f,"l %d %d\n", R->noeuds->nd->voisins->nd->num, R->noeuds->nd->num);
+		}
+	}
+	fprintf(f,"\n");
+
+	for(int i = 0; i < nbCommodite; i++){
+		fprintf(f,"k %d %d\n", R->commodites->extrA->num, R->commodites->extrB->num);
+	}
+}
+void afficheReseauSVG(Reseau *R, char* nomInstance){
+    CellNoeud *courN,*courv;
+    SVGwriter svg;
+    double maxx=0,maxy=0,minx=1e6,miny=1e6;
+
+    courN=R->noeuds;
+    while (courN!=NULL){
+        if (maxx<courN->nd->x) maxx=courN->nd->x;
+        if (maxy<courN->nd->y) maxy=courN->nd->y;
+        if (minx>courN->nd->x) minx=courN->nd->x;
+        if (miny>courN->nd->y) miny=courN->nd->y;
+        courN=courN->suiv;
+    }
+    SVGinit(&svg,nomInstance,500,500);
+    courN=R->noeuds;
+    while (courN!=NULL){
+        SVGpoint(&svg,500*(courN->nd->x-minx)/(maxx-minx),500*(courN->nd->y-miny)/(maxy-miny));
+        courv=courN->nd->voisins;
+        while (courv!=NULL){
+            if (courv->nd->num<courN->nd->num)
+                SVGline(&svg,500*(courv->nd->x-minx)/(maxx-minx),500*(courv->nd->y-miny)/(maxy-miny),500*(courN->nd->x-minx)/(maxx-minx),500*(courN->nd->y-miny)/(maxy-miny));
+            courv=courv->suiv;
+        }
+        courN=courN->suiv;
+    }
+    SVGfinalize(&svg);
+}
