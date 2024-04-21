53cc6fd086eb7ec75871dcb2e64c570f67127b85
IBZua <120250986+IBZua@users.noreply.github.com>
2024-04-21 17:03:16 +0200

    Update and rename reseau.c to Reseau.c
    maj et ajout de commentaire:
    -rechercheCreeNoeudListe
    -nbCommodites
    -nbLiaisons
    -ecrireReseau

--- Reseau.c
+++ Reseau.c
@@ -1,25 +1,38 @@
 #include "Reseau.h"
-#include "Chaine.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 Noeud* rechercheCreeNoeudListe(Reseau *R, double x, double y){
+	//si reseau vide
+	if(!R){return 0;}
+	//On parcourt chaque noeud du reseau tant que les coordonnees correspondent pas
 	while(R->noeuds->nd && R->noeuds->nd->x != x && R->noeuds->nd->y != y){
 		R->noeuds = R->noeuds->suiv;
 	}
+	//Si rien n'est trouve on cree le noeud aux coordonnees x y
 	if(!R->noeuds->nd){
+		//maj du nombre de noeud
 		R->nbNoeuds++;
-		Noeud* n = malloc(sizeof(Noeud));
+		//creation nouveau noeud
+		Noeud* n = (Noeud*)malloc(sizeof(Noeud));
 		n->x = x;
 		n->y = y;
 		n->num = R->nbNoeuds;
+		n->voisins = NULL;
 		R->noeuds->suiv->nd = n;
+		//creation nouvelle cellnoeud
+		CellNoeud* cn = (CellNoeud*)malloc(sizeof(CellNoeud));
+		cn->nd = n;
+		cn->suiv = R->noeuds;
+		R->noeuds = cn;
 		return n;
 	}
 	return R->noeuds->nd;
 }
 
 Reseau* reconstitueReseauListe(Chaines *C){
+	//si chaine vide
+	if(!C){return NULL;}
+	
 	Reseau* R = malloc(sizeof(Reseau));
 	R->nbNoeuds = comptePointsTotal(C);
 	R->gamma = C->gamma;
@@ -33,15 +46,20 @@ Reseau* reconstitueReseauListe(Chaines *C){
 		prec = C->chaines->points;
 		C->chaines->points = C->chaines->points->suiv;
 		
-		while(C->chaines->points->suiv != NULL){
+		while(C->chaines->points){
+			if(C->chaines->points->suiv != NULL){
 				R->noeuds->nd = rechercheCreeNoeudListe(R, C->chaines->points->x, C->chaines->points->y);
 				R->noeuds->nd->voisins->nd = rechercheCreeNoeudListe(R, prec->x, prec->y);
 				R->noeuds->nd->voisins->suiv->nd = rechercheCreeNoeudListe(R, C->chaines->points->suiv->x, C->chaines->points->suiv->y);
 				prec = C->chaines->points;
 				C->chaines->points = C->chaines->points->suiv;
-		
+			}
+			else{
+				break;
+			}
 		}
 		
+		C->chaines->points = C->chaines->points->suiv;
 		R->commodites->extrB = rechercheCreeNoeudListe(R, C->chaines->points->x, C->chaines->points->y);
 		R->commodites = R->commodites->suiv;
 		C->chaines = C->chaines->suiv;
@@ -50,6 +68,8 @@ Reseau* reconstitueReseauListe(Chaines *C){
 }
 
 int nbCommodites(Reseau *R){
+	//si reseau vide
+	if(!R){return 0;}
 	int cpt = 0;
 	while(R->commodites){
 		cpt++;
@@ -59,9 +79,19 @@ int nbCommodites(Reseau *R){
 }
 
 int nbLiaisons(Reseau*R){
+	//si reseau vide
+	if(!R){return 0;}
 	int cpt = 0;
+	//on check chaque noeud
 	while(R->noeuds){
-		cpt++;
+		//on check chaque liaison du noeud
+		while(R->noeuds->nd->voisins){
+			//on filtre les liaisons pour eviter les doublons
+			if(R->noeuds->nd->voisins->nd->num < R->noeuds->nd->num){
+				cpt++;
+			}
+			R->noeuds->nd->voisins = R->noeuds->nd->voisins->suiv;
+		}
 		R->noeuds = R->noeuds->suiv;
 	}
 	return cpt;
@@ -81,13 +111,19 @@ void ecrireReseau(Reseau*R,FILE*f){
 	}
 	fprintf(f,"\n");
 	//la liste des voisins de chaque noeud
+	//On parcourt la liste des noeuds du reseau
 	for(int i = 0; i < R->nbNoeuds; i++){
+		//on parcourt la liste des voisins de chaque noeud
 		while(R->noeuds->nd->voisins){
-			fprintf(f,"l %d %d\n", R->noeuds->nd->voisins->nd->num, R->noeuds->nd->num);
+			//si le num du voisin est inf au num du noeud on ecrit
+			if(R->noeuds->nd->voisins->nd->num < R->noeuds->nd->num){
+				fprintf(f,"l %d %d\n", R->noeuds->nd->voisins->nd->num, R->noeuds->nd->num);
+			}
+			R->noeuds->nd->voisins = R->noeuds->nd->voisins->suiv;
 		}
 	}
 	fprintf(f,"\n");
-
+	//la liste des commodites
 	for(int i = 0; i < nbCommodite; i++){
 		fprintf(f,"k %d %d\n", R->commodites->extrA->num, R->commodites->extrB->num);
 	}
