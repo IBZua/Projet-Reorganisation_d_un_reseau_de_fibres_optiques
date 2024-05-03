8bce30e39e9dd369bcbcc04ef7dcfe5604294c47
IBZua <120250986+IBZua@users.noreply.github.com>
2024-05-03 16:51:44 +0200

    Update Hachage.h

--- Hachage.h
+++ Hachage.h
@@ -1,5 +1,10 @@
+#include "Chaine.h"
+#include "Reseau.h"
 typedef struct{
   int nbElement; //pas necessaire ici
   int tailleMax;
   CellNoeud** T;
 } TableHachage ;
+
+Reseau* reconstitueReseauHachage(Chaines *C, int m);
+void libereTableHachage(TableHachage * TH);
